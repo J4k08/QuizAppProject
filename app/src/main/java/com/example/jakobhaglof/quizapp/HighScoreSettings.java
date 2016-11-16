160c33a386b1edf77c2d87c87fa2f50a2ccb1b06
Jakob Haglöf <j.haglof56@gmail.com>
2016-11-16 12:13:51 +0100

    initiate project

---
+++ app/src/main/java/com/example/jakobhaglof/quizapp/HighScoreSettings.java
@@ -0,0 +1,18 @@
+package com.example.jakobhaglof.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighScoreSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score_settings);
+    }
+
+    public void importantMethod() {
+
+        String hej = "Jakob";
+    }
+}
