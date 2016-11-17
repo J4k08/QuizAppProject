17b64179e34679af24966ac033b88fa97e984d90
Christoffer Arvidsson <Curject@Apples-MacBook-Pro.local>
2016-11-17 11:29:27 +0100

    Changed Name on Intro Activity to Splash

--- app/src/main/java/com/example/jakobhaglof/quizapp/Splash.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/Splash.java
@@ -3,11 +3,11 @@ package com.example.jakobhaglof.quizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class IntroActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.splashh);
     }
 }
