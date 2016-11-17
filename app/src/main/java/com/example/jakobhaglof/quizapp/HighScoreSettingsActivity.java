b8aeebc420d9d71669e36ddc7dd62f6feb2762bf
Jakob Haglöf <j.haglof56@gmail.com>
2016-11-17 13:32:14 +0100

    added things

--- app/src/main/java/com/example/jakobhaglof/quizapp/HighScoreSettingsActivity.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/HighScoreSettingsActivity.java
@@ -3,7 +3,7 @@ package com.example.jakobhaglof.quizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HighScoreSettings extends AppCompatActivity {
+public class HighScoreSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,8 +11,4 @@ public class HighScoreSettings extends AppCompatActivity {
         setContentView(R.layout.activity_high_score_settings);
     }
 
-    public void importantMethod() {
-
-        String hej = "Jakob";
-    }
 }
