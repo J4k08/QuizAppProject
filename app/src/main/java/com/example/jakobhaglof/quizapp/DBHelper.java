17b60092c9daeb58b70123d4a516126915342da2
Jakob Haglöf <j.haglof56@gmail.com>
2016-11-18 13:38:23 +0100

    Added method for adding question and made changes to player-table

--- app/src/main/java/com/example/jakobhaglof/quizapp/DBHelper.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/DBHelper.java
@@ -1,5 +1,6 @@
 package com.example.jakobhaglof.quizapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,8 +12,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final String db_name = "myDataBase";
-
     private SQLiteDatabase dbase;
+
+    private static final String ID = "QuestionID";
+    private static final String QUEST = "question";
+    private static final String CATEGORY = "category";
+    private static final String CORRECT = "correct";
+    private static final String CHOICE1 = "choice1";
+    private static final String CHOICE2 = "choice2";
+    private static final String CHOICE3 = "choice3";
+    private static final String CHOICE4 = "choice4";
+
+    private static final String P_ID = "pId";
+    private static final String P_NAME = "name";
+    private static final String HIGHSCORE = "highscore";
 
     public DBHelper(Context context) {
         super(context, db_name, null, 1);
@@ -22,27 +35,29 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String sqlPlayer = "CREATE TABLE players (" +
-         "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-         "name VARCHAR(255) NOT NULL," +
-         "highscore INTEGER" +
+
+         P_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+         P_NAME + " VARCHAR(255) NOT NULL," +
+         HIGHSCORE + " INTEGER" +
          ")";
 
         sqLiteDatabase.execSQL(sqlPlayer);
 
         String sqlQuestions ="CREATE TABLE questions ("+
 
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "question VARCHAR(255) NOT NULL," +
-                "category VARCHAR(255) NOT NULL," +
-                "correct VARCHAR(255) NOT NULL," +
-                "choice1 VARCHAR(255) NOT NULL, " +
-                "choice2 VARCHAR(255) NOT NULL, " +
-                "choice3 VARCHAR(255) NOT NULL, " +
-                "choice4 VARCHAR(255) NOT NULL)";
+                ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                QUEST +" VARCHAR(255) NOT NULL," +
+                CATEGORY +" VARCHAR(255) NOT NULL," +
+                CORRECT +" VARCHAR(255) NOT NULL," +
+                CHOICE1 +" VARCHAR(255) NOT NULL, " +
+                CHOICE2 +" VARCHAR(255) NOT NULL, " +
+                CHOICE3 +" VARCHAR(255) NOT NULL, " +
+                CHOICE4 +" VARCHAR(255) NOT NULL)";
 
 
 
         sqLiteDatabase.execSQL(sqlQuestions);
+        addQuestion();
 
 
         dbase.close();
@@ -51,6 +66,20 @@ public class DBHelper extends SQLiteOpenHelper {
     public void addQuestion() {
 
         Questions q1 = new Questions("What is 2 + 2?", "Math", "4", "1", "2", "4", "300");
+        this.addQuestion(q1);
+
+    }
+    public void addQuestion(Questions quest) {
+
+        dbase = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(QUEST, quest.getQuestion());
+        values.put(CATEGORY, quest.getCategory());
+        values.put(CORRECT, quest.getCorrectAnswer());
+        values.put(CHOICE1, quest.getChoice1());
+        values.put(CHOICE2, quest.getChoice2());
+        values.put(CHOICE3, quest.getChoice3());
+        values.put(CHOICE4, quest.getChoice4());
 
     }
 
