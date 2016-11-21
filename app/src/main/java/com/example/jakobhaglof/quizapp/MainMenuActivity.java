72b458e899be480c0841c4218edd3b8bca8f17c2
Samuela Lundborg <sam-lundborg@hotmail.com>
2016-11-21 21:52:53 +0100

    made main menu xml design with onClick on buttons

--- app/src/main/java/com/example/jakobhaglof/quizapp/MainMenuActivity.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/MainMenuActivity.java
@@ -36,17 +36,17 @@ public class MainMenuActivity extends AppCompatActivity {
             startActivity(go2GameIntent);
             return true;
         } else if (id == R.id.info) {
-            Toast.makeText(this, "Going to about",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Going to about", Toast.LENGTH_SHORT).show();
             Intent go2AboutIntent = new Intent(this, AboutActivity.class);
             startActivity(go2AboutIntent);
             return true;
         } else if (id == R.id.highscores) {
-            Toast.makeText(this, "Going to highscores",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Going to highscores", Toast.LENGTH_SHORT).show();
             Intent go2HighscoresIntent = new Intent(this, HighScoreActivity.class);
             startActivity(go2HighscoresIntent);
             return true;
         } else if (id == R.id.settings) {
-            Toast.makeText(this, "Going to settings",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Going to settings", Toast.LENGTH_SHORT).show();
             Intent go2SettingsIntent = new Intent(this, SettingsActivity.class);
             startActivity(go2SettingsIntent);
             return true;
@@ -57,7 +57,19 @@ public class MainMenuActivity extends AppCompatActivity {
 
     public void sendToGame(View view) {
 
-        Intent intent = new Intent(this,GameActivity.class);
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void sendToProfiles(View view) {
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void sendToSettings(View view) {
+
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 }
