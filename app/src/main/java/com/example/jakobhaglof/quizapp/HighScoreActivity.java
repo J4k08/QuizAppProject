ecc163612473d95950ce691b7640db4e9c9a929f
Jakob Haglöf <j.haglof56@gmail.com>
2016-11-21 10:37:48 +0100

    ready for merge

--- app/src/main/java/com/example/jakobhaglof/quizapp/HighScoreActivity.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/HighScoreActivity.java
@@ -11,7 +11,7 @@ public class HighScoreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_high_score_settings);
+        setContentView(R.layout.activity_high_score);
     }
 
     @Override
