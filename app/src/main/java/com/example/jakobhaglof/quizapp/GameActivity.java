61c52835d3a9e787bff6d902c133fda4d7068847
Jakob Haglöf <j.haglof56@gmail.com>
2016-11-29 15:41:05 +0100

    First attempt at gameLogic (chunky)

--- app/src/main/java/com/example/jakobhaglof/quizapp/GameActivity.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/GameActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,9 +16,18 @@ public class GameActivity extends AppCompatActivity {
 
     private final static String TAG = "GAME_ACTIVITY: ";
     DBHelper db = new DBHelper(this);
+    int clickCounter = 0;
     Player player;
     String pName = "";
-    ArrayList<String> clicked;
+    ArrayList<String> clickedCat;
+    ArrayList<Question> gameQuestions;
+    TextView que = (TextView)findViewById(R.id.setQuestion);
+    Button btn1 = (Button)findViewById(R.id.btnChoice1);
+    Button btn2 = (Button)findViewById(R.id.btnChoice2);
+    Button btn3 = (Button)findViewById(R.id.btnChoice3);
+    Button btn4 = (Button)findViewById(R.id.btnChoice4);
+    Game game;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,18 @@ public class GameActivity extends AppCompatActivity {
         player = db.getPlayerFromDB(pName = i.getStringExtra("pName"));
 
         Log.d(TAG, player.getName());
-        clicked = (ArrayList<String>) getIntent().getSerializableExtra("clicked");
+        clickedCat = (ArrayList<String>)getIntent().getSerializableExtra("clickedCat");
+        game = new Game(100, clickedCat , player);
+
+        gameQuestions = game.prepGame(clickedCat);
+
+        while(clickCounter < 11) {
+
+            playGame(gameQuestions);
+
+        }
+
+
 
     }
     @Override
@@ -50,4 +73,25 @@ public class GameActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void playGame (ArrayList<Question> questions) {
+
+            que.setText(questions.get(clickCounter).getQuestion());
+            btn1.setText(questions.get(clickCounter).getChoice1());
+            btn2.setText(questions.get(clickCounter).getChoice2());
+            btn3.setText(questions.get(clickCounter).getChoice3());
+            btn4.setText(questions.get(clickCounter).getChoice4());
+
+        }
+
+    public void categoryBoxes(View view) {
+
+        Question q = new Question();
+        q = gameQuestions.get(clickCounter);
+        String guess = view.toString();
+
+        game.roundGuess(guess, q, player);
+
+        clickCounter ++;
+
+    }
 }
