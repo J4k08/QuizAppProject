76e3567566459f09868cb870065003c5ccced25c
Jakob Haglöf <j.haglof56@gmail.com>
2016-12-06 14:10:51 +0100

    Added results on a screen, after the game

--- app/src/main/java/com/example/jakobhaglof/quizapp/PostGameActivity.java
+++ app/src/main/java/com/example/jakobhaglof/quizapp/PostGameActivity.java
@@ -3,24 +3,46 @@ package com.example.jakobhaglof.quizapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.security.acl.LastOwnerException;
+import java.util.ArrayList;
 
 public class PostGameActivity extends AppCompatActivity {
 
     private final static String TAG = "POST_GAME_ACTIVITY: ";
-    DBHelper db;
-    Player player;
-    String pName = "";
+    private DBHelper db;
+    private Player player;
+    private String pName = "";
+    private int lastRoundScore;
+    private ArrayList<String> clickedCat;
+    private String playedCat;
+
+    private TextView TVplayerName;
+    private TextView TVplayedCat;
+    private TextView TVroundScore;
+    private TextView TVplayerHighScore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_game);
         db = new DBHelper(this);
+
         Intent i = getIntent();
+        lastRoundScore = i.getExtras().getInt("playerScore");
+        Log.d(TAG, "onCreate: " + lastRoundScore);
+        clickedCat = i.getStringArrayListExtra("clickedCat");
         player = db.getPlayerFromDB(pName = i.getExtras().getString("pName"));
+        playedCat = displayCategories(clickedCat);
+
+        displayResult();
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,5 +78,42 @@ public class PostGameActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public String displayCategories(ArrayList<String> clickedCat) {
+
+        playedCat = "";
+
+        if(clickedCat.size() == 4) {
+
+            playedCat = "Alla";
+
+        } else {
+
+            playedCat = clickedCat.get(0);
+            for (int i = 1; i < clickedCat.size(); i++) {
+
+
+                playedCat = playedCat + " & " + clickedCat.get(i);
+
+            }
+        }
+        return playedCat;
+    }
+
+    public void displayResult() {
+
+        TVplayerName = (TextView)findViewById(R.id.writePlayerName);
+        TVplayerName.setText(player.getName());
+
+        TVplayedCat = (TextView)findViewById(R.id.writePlayedCategory);
+        TVplayedCat.setText(playedCat);
+
+        TVroundScore = (TextView)findViewById(R.id.writePointsGame);
+        TVroundScore.setText("" + lastRoundScore);
+
+        TVplayerHighScore = (TextView)findViewById(R.id.writePlayerHighScore);
+        TVplayerHighScore.setText("" + player.getHighScore());
+
     }
 }
